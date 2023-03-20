f6cb2c5a969eddb2c7ac3841805a1187f65880bc
Sublimeful <Qiangwu2000@gmail.com>
2023-03-20 11:54:47 -0500

    Updated pearl algorithm to include position check

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,5 @@
 from typing import Dict, List
-from datamodel import OrderDepth, TradingState, Order
+from datamodel import OrderDepth, Position, TradingState, Order
 
 
 class Trader:
@@ -16,8 +16,8 @@ class Trader:
                 order_depth: OrderDepth = state.order_depths[product]
                 orders: list[Order] = []
 
-                acceptable_buy_price = 9997
-                acceptable_sell_price = 10003
+                acceptable_buy_price = 9999
+                acceptable_sell_price = 10001
 
                 # We buy here
                 # We look at SELL orders (Negative volume)
@@ -25,7 +25,9 @@ class Trader:
                     best_ask = min(order_depth.sell_orders.keys())
                     best_ask_volume = order_depth.sell_orders[best_ask]
 
-                    if best_ask <= acceptable_buy_price:
+                    # Makes sure that the ask is less than or equal to our acceptable_buy_price
+                    # Also makes sure that our position is within range
+                    if best_ask <= acceptable_buy_price and state.position[product] >= -20 and state.position[product] <= 20:
                         print("BUY", str(-best_ask_volume) + "x", best_ask)
                         orders.append(Order(product, best_ask, -best_ask_volume))
 
@@ -38,7 +40,9 @@ class Trader:
                     best_bid = max(order_depth.buy_orders.keys())
                     best_bid_volume = order_depth.buy_orders[best_bid]
 
-                    if best_bid >= acceptable_sell_price:
+                    # Makes sure that the bid is greater than or equal to our acceptable_sell_price,
+                    # Also makes sure that our position is within range
+                    if best_bid >= acceptable_sell_price and state.position[product] >= -20 and state.position[product] <= 20:
                         print("SELL", str(best_bid_volume) + "x", best_bid)
                         orders.append(Order(product, best_bid, -best_bid_volume))
 
