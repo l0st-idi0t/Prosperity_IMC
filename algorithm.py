338b315687fb5bd4966f2838fa72f2091bd02a71
Sublimeful <Qiangwu2000@gmail.com>
2023-03-22 00:50:44 -0500

    Added banana

--- algorithm.py
+++ algorithm.py
@@ -48,6 +48,40 @@ class Trader:
         # List of all orders we make
         orders: list[Order] = []
 
+        m = 8.721608721608722e-07;
+        b = 4891.3684355644355;
+        bias = 25
+
+        time = state.timestamp;
+
+        acceptable_buy_price = m * time + b - bias;
+        acceptable_sell_price = m * time + b + bias;
+
+        if state.position.get(product, 0) <= 0:
+            # Position is negative, We buy here
+            # We look at SELL orders (Negative volume)
+            if len(order_depth.sell_orders) > 0:
+                best_ask = min(order_depth.sell_orders.keys())
+                best_ask_volume = order_depth.sell_orders[best_ask]
+
+                # Makes sure that the ask is less than or equal to our acceptable_buy_price
+                if best_ask <= acceptable_buy_price:
+                    orders.append(Order(product, best_ask, -best_ask_volume))
+        else:
+            # Position is positive, We sell here
+            # We look at BUY orders (Positive volume)
+            if len(order_depth.buy_orders) > 0:
+                best_bid = max(order_depth.buy_orders.keys())
+                best_bid_volume = order_depth.buy_orders[best_bid]
+
+                # Makes sure that the bid is greater than or equal to our acceptable_sell_price,
+                if best_bid >= acceptable_sell_price:
+                    orders.append(Order(product, best_bid, -best_bid_volume))
+
+        return orders;
+
+
+
         
 
 
